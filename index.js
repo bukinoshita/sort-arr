e59a9c08fdb8817887630ff987b43573a0490fc3
Gabriel Pedro <gpedro831@gmail.com>
2017-07-13 02:30:51 -0400

    feat: reducing size

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 'use strict'
 
-module.exports = (arr = [], k) => {
-  if (k) {
-    return arr.sort((a, b) => {
-      return a[k] > b[k] ? 1 : -1
-    })
-  }
-
-  return arr.sort()
-}
+module.exports = (arr = [], k) => arr.sort(k ? (a, b) => a[k] > b[k] ? 1 : -1 : undefined)
